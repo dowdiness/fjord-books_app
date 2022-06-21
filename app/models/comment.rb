cab4279a58581184d4fd323b34906b4432d4ee42
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-06-21 21:13:33 +0900

    add author? method to comment and report model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,10 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  cattr_accessor :current_user, instance_accessor: false
+
+  def author?
+    @@current_user.id == user_id
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy, as: :commentable
+
+  cattr_accessor :current_user, instance_accessor: false
+
+  def author?
+    @@current_user.id == user_id
+  end
 end
