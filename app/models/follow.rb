d92b424339360f11f91a3340f42fee0ca048c103
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-06-22 23:13:28 +0900

    use custom method for validation

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-class FollowValidator < ActiveModel::Validator
-  def validate(record)
-    return unless record.follower_id == record.following_id
-
-    record.errors.add :base, :invalid, message: '自分へのフォローは出来ません'
-  end
-end
-
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :following, class_name: 'User'
   # 自分へのフォローは出来ない
   validates :follower_id, presence: true
   validates :following_id, presence: true
-  validates_with FollowValidator
+  validate :cannot_follow_myself
+
+  def cannot_follow_myself
+    return if follower_id != following_id
+
+    errors.add :base, :invalid, message: '自分へのフォローは出来ません'
+  end
 end
