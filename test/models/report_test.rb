ab711ad5128c3b24bf8de98e320837eb277fc7b2
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-06-30 19:06:14 +0900

    add model tests

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,32 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = create(:report)
+  end
+
+  test 'report should be valid' do
+    assert @report.valid?
+  end
+
+  test 'title should not be blank' do
+    @report.title = ''
+    assert_not @report.valid?
+  end
+
+  test 'content should not be blank' do
+    @report.content = ''
+    assert_not @report.valid?
+  end
+
+  test 'editable?' do
+    target_user = @report.user
+    other_user = create(:user)
+    assert @report.editable?(target_user)
+    assert_not @report.editable?(other_user)
+  end
+
+  test 'created_on' do
+    assert_equal @report.created_on, @report.created_at.to_date
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    @other_user = create(:user)
+  end
+
+  test 'name_or_email with name' do
+    assert @user.name_or_email, @user.name
+  end
+
+  test 'name_or_email without name' do
+    noname_user = create(:user, name: nil)
+    assert_equal(noname_user.name_or_email, noname_user.email)
+  end
+
+  test 'folllowing? and followed_by?' do
+    @user.active_relationships.find_or_create_by!(following_id: @other_user.id)
+    assert(@user.following?(@other_user))
+    assert(@other_user.followed_by?(@user))
+  end
+
+  test 'follow and unfollow' do
+    @user.follow @other_user
+    assert @user.following?(@other_user)
+    @user.unfollow @other_user
+    assert_not @user.following?(@other_user)
+  end
 end
